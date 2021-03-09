e066bf8d048d9e325377d76d50971f191adfbc34
Mars SHI <35283331+mars0315@users.noreply.github.com>
2021-03-10 00:20:49 +0800

    Add files via upload

---
+++ src/test/java/com/mars/test/testLeetCode/Test0307Palindrome.java
@@ -0,0 +1,26 @@
+package testDemo;
+
+public class Test0307Palindrome {
+    public static void main(String[] args) {
+        boolean bl=testPalindrome(1221);
+        System.out.println(bl);
+    }
+
+    /**
+     * 给你一个整数 x ，如果 x 是一个回文整数，返回 true ；否则，返回 false 。
+     * @param x
+     */
+    public static boolean testPalindrome(int x) {
+        //首先读取数x，将x转为字符串
+        String str1=String.valueOf(x);
+        //将字符串反转
+        StringBuffer sb=new StringBuffer(str1);
+        String str2=sb.reverse().toString();
+        //否则返回false
+        if (!str1.equals(str2)) {
+            return false;
+        }
+        //如果x和反转后的数相等则返回true
+        return true;
+    }
+}
