d1fc48e76a3d5346d6281a9885ccfe783a74b7fa
Your Name <yourmail@decathlon.com>
2021-03-19 15:31:48 +0800

    20210319_2

--- src/test/java/com/mars/test/testLeetCode/Test20210319SearchInsert.java
+++ src/test/java/com/mars/test/testLeetCode/Test20210319SearchInsert.java
@@ -1,4 +1,27 @@
 package com.mars.test.testLeetCode;
 
 public class Test20210319SearchInsert {
+    public static void main(String[] args) {
+        searchInsert(new int[]{1,3,5,6},2);
+    }
+
+    /**
+     * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+     *
+     * 你可以假设数组中无重复元素。
+     * @param nums,target
+     */
+    public static int searchInsert(int[] nums,int target) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        for (int i = 0; i <nums.length ; i++) {
+            if (nums[i] < target) {
+                continue;
+            }else{
+                return i;
+            }
+        }
+        return nums.length+1;
+    }
 }
